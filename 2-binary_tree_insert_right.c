e2972b35036365af8796297bc3aa45eaa43bebd4
amine784 <1066@holbertonschool.com>
2020-03-11 17:18:25 +0100

    holberto

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,13 +13,13 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 binary_tree_t *node = malloc(sizeof(binary_tree_t));
 if (node == NULL || parent == NULL)
 return (NULL);
-(*node).n = value;
 (*node).parent = parent;
-if ((*parent).right != NULL)
-{
+(*node).n = value;
 (*node).right = (*parent).right;
-(*parent).right->parent = node;
-}
 (*parent).right = node;
+if ((*node).right != NULL)
+{
+(*node).right->parent = node;
+}
 return (node);
 }
