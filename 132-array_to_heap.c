a83747d31296cb214b35f46a1a44d1fd1b80fc42
boumaiza-kais <1110@holbertonschool.com>
2020-03-12 21:32:56 +0100

    task 132

---
+++ 132-array_to_heap.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - function that builds a Max Binary 
+ * Heap tree from an array
+ *
+ * Return: nothing
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+
+}
