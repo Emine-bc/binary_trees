36a42d2f9c3af84d3e4732eeca5899611265b2d6
boumaiza-kais <1110@holbertonschool.com>
2020-03-12 21:27:16 +0100

    task 124

---
+++ 124-sorted_array_to_avl.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_heap - function that builds an AVL tree from an array
+ *
+ * Return: nothing
+ */
+int binary_tree_is_heap(const binary_tree_t *tree)
+{
+
+}
