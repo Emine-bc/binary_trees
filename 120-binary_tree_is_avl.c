5b2d6c620cbed91c65a6c869e0ae672e4783779b
boumaiza-kais <1110@holbertonschool.com>
2020-03-12 21:21:00 +0100

    task 120

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * avl_insert - function that checks if a binary tree is a valid AVL Tree
+ *
+ * Return: nothing
+ */
+avl_t *avl_insert(avl_t **tree, int value)
+{
+
+}
