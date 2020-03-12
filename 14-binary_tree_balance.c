f39ab343766f422d37c4d0220d60338da5f1b992
amine784 <1066@holbertonschool.com>
2020-03-12 14:11:48 +0100

    holberto

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,8 +11,24 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 if (tree ==  NULL)
 return (0);
-if ((*tree).right == NULL)
 return (binary_tree_height((*tree).left) - binary_tree_height((*tree).right));
-return (binary_tree_height((*tree).left) -
-	binary_tree_height((*tree).right) - 1);
+}
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ *@tree: Pointer
+ * Return: measure
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+int measure_r = 0, measure_l = 0;
+if (tree == NULL)
+return (-1);
+if (tree->right)
+measure_r = binary_tree_height(tree->right) + 1;
+if (tree->left)
+measure_l = binary_tree_height(tree->left) + 1;
+if (measure_r > measure_l)
+return (measure_r);
+else
+return (measure_l);
 }
