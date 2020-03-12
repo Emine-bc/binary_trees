bcf9bd4146ec693aa8bb4baf037386e9e5a726d5
boumaiza-kais <1110@holbertonschool.com>
2020-03-12 21:25:46 +0100

    task 123

---
+++ 123-avl_remove.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * sorted_array_to_avl - function that removes a node from an AVL tree
+ *
+ * Return: nothing
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+
+}
