987c06f9164ba20695aefb5cbcc605a1f680a0cc
boumaiza-kais <1110@holbertonschool.com>
2020-03-12 21:40:48 +0100

    task 134

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * *heap_to_sorted_array - function that converts a Binary Max Heap 
+ * to a sorted array of integers 
+ *
+ * Return: nothing
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+
+}
