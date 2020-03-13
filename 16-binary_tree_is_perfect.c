2fe0fbaf1a196fddfadc2dc18f45d200d54828b6
amine784 <1066@holbertonschool.com>
2020-03-13 11:59:05 +0100

    new push

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
+size_t binary_tree_depth(const binary_tree_t *tree);
+/**
+*check-function that hel to checks
+*@tree:is a pointer to the root node of the tree
+*@count:is a compteur
+*@i: variable help check
+*Return: always success.
+**/
+size_t check(const binary_tree_t *tree, size_t count, size_t i)
+{
+int x = 0;
+int y = 0;
+if (tree == NULL)
+return (0);
+if ((*tree).left == NULL)
+{
+if ((*tree).right == NULL)
+return (count == i + 1);
+}
+if (!(*tree).right)
+return (0);
+x = check((*tree).left, count, i + 1);
+y = check((*tree).right, count, i + 1);
+return (x *y);
+}
+/**
+*binary_tree_is_perfect-function that checks if a binary tree is perfect
+*@tree:is a pointer to the root node of the tree to check
+*Return: always success.
+**/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+int result = 0, nil = 0;
+if (!tree)
+return (0);
+size_t count = binary_tree_depth(tree);
+result = check(tree, count, nil);
+return (result);
+}
 /**
 * binary_tree_depth-unction that measures the depth
 * of a node in a binary tree
@@ -9,40 +48,8 @@
 **/
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-int count;
+size_t count;
 for (count = 0; tree != NULL; count++)
 tree = (*tree).left;
 return (count);
-}
-/**
-*check-function that hel to checks
-*@tree:is a pointer to the root node of the tree
-*@count:is a compteur
-*@i: variable help check
-*Return: always success.
-**/
-int check(const binary_tree_t *tree, int count, int i)
-{
-int x = 0;
-int y = 0;
-if (tree == NULL)
-return (0);
-if (!(*tree).left && !(*tree).right)
-return (count == i + 1);
-if (!(*tree).left || !(*tree).right)
-return (0);
-x = check((*tree).left, count, i + 1);
-y = check((*tree).right, count, i + 1);
-return (x *y);
-}
-
-/**
-*binary_tree_is_perfect-function that checks if a binary tree is perfect
-*@tree:is a pointer to the root node of the tree to check
-*Return: always success.
-**/
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-int count = binary_tree_depth(tree);
-return (check(tree, count, 0));
 }
