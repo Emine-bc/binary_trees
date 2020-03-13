f8cc9c9978dc6d15b55092632ae7846c0af49fc0
amine784 <1066@holbertonschool.com>
2020-03-13 12:28:18 +0100

    holberto

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
+int check(const binary_tree_t *tree, int count, int i);
 /**
 * binary_tree_depth-unction that measures the depth
 * of a node in a binary tree
@@ -15,6 +16,18 @@ tree = (*tree).left;
 return (count);
 }
 /**
+*binary_tree_is_perfect-function that checks if a binary tree is perfect
+*@tree:is a pointer to the root node of the tree to check
+*Return: always success.
+**/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+int result = 0, nil = 0;
+int count = binary_tree_depth(tree);
+result = check(tree, count, nil);
+return (result);
+}
+/**
 *check-function that hel to checks
 *@tree:is a pointer to the root node of the tree
 *@count:is a compteur
@@ -27,8 +40,11 @@ int x = 0;
 int y = 0;
 if (tree == NULL)
 return (0);
-if (!(*tree).left && !(*tree).right)
+if ((*tree).left ==  NULL)
+{
+if ((*tree).right == NULL)
 return (count == i + 1);
+}
 if (!(*tree).left || !(*tree).right)
 return (0);
 x = check((*tree).left, count, i + 1);
@@ -36,15 +52,3 @@ y = check((*tree).right, count, i + 1);
 return (x *y);
 }
 
-/**
-*binary_tree_is_perfect-function that checks if a binary tree is perfect
-*@tree:is a pointer to the root node of the tree to check
-*Return: always success.
-**/
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-int result = 0, nil = 0;
-int count = binary_tree_depth(tree);
-result = check(tree, count, nil);
-return (result);
-}
