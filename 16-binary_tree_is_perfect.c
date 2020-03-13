1d74b170eedc29c5ef8b3916a345ad675d6be67f
amine784 <1066@holbertonschool.com>
2020-03-13 09:15:56 +0100

    holberto

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,33 +2,75 @@
 #include <stdio.h>
 #include "binary_trees.h"
 /**
-*check-function that hel to checks
-*@tree:is a pointer to the root node of the tree
-*Return: always success.
-**/
-int check(binary_tree_t *tree)
+ * binary_tree_is_leaf - node is a leaf
+ * Return: 1 or 0
+ * @node: pointer
+ *
+ *
+ **/
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-int right = 0;
-int left = 0;
-int check_value = 0;
-if (tree == NULL)
-return (0);
-check_value = ((right = 1 + check((*tree).right))) +
-(left = 1 + check((*tree).left));
-return (check_value);
+	int x = 0;
+	if (node && !(node->left) && !(node->right))
+		x = 1;
+	return (x);
+}
+
+
+/**
+ * binary_tree_is_parent_full - node is a parent
+ * Return: 1 or 0
+ * @node: pointer
+ *
+ *
+ **/
+int binary_tree_is_parent_full(const binary_tree_t *node)
+{
+	int y = 0;
+	if (node && node->left && node->right)
+		y = 1;
+	return (y);
 }
 /**
-*binary_tree_is_perfect-function that checks if a binary tree is perfect
-*@tree:is a pointer to the root node of the tree to check
-*Return: always success.
-**/
+ * height - height of a tree
+ * Return: height
+ * @tree: tree
+ *
+ *
+ **/
+int height(const binary_tree_t *tree)
+{
+	int x1 = 0;
+	int x2 = 0;
+
+	if (tree == NULL)
+		return (-1);
+	x1 = height(tree->left);
+	x2 = height(tree->right);
+	if (x1 > x2)
+		return (x1 + 1);
+	return (x2 + 1);
+}
+/**
+ * binary_tree_is_perfect - binary tree is perfect
+ * Return: 1 or 0
+ * @tree: tree
+ *
+ *
+ **/
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-if (tree == NULL)
-return (0);
-int right = check((*tree).right);
-int left = check((*tree).left);
-if (right != left)
-return (0);
-return (1);
+	if (tree && height(tree->left) == height(tree->right))
+	{
+		if (height(tree->left) == -1)
+			return (1);
+		if (binary_tree_is_leaf(tree->left) && binary_tree_is_leaf(tree->right))
+			return (1);
+
+		if (binary_tree_is_parent_full(tree))
+			return (binary_tree_is_perfect(tree->left) &&
+				binary_tree_is_perfect(tree->right));
+	}
+
+	return (0);
 }
